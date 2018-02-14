9b43d0e6f4a934bf038846db86e9033d55bf4366
Graeme Coupar <grambo@grambo.me.uk>
2018-02-14 23:37:58 +0000

    Add mix format config.

---
+++ .formatter.exs
@@ -0,0 +1,17 @@
+[
+  # The maximum line length to allow.
+  line_length: 80,
+
+  # The files to format by default
+  inputs: "{config,lib,test}/**/*.{ex,exs}",
+
+  # Which libraries should we import formatting rules from.
+  # Note: At the time of writing, the released versions of some of these do not
+  # expose formatting rules.
+  import_deps: [],
+
+  # Which local function calls (that is, calls without a module name) should not
+  # be parenthesised.
+  locals_without_parens: [ ]
+]
+
