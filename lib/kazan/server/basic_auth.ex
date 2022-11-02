11b2929760e04be821679d497bd79cd9ffc7cb71
Pedro Vieira <pedro@vieira.net>
2022-11-02 10:38:58 -0300

    Update basic_auth.ex

--- lib/kazan/server/basic_auth.ex
+++ lib/kazan/server/basic_auth.ex
@@ -5,13 +5,13 @@ defmodule Kazan.Server.BasicAuth do
           token: binary
         }
 
-  defimpl Inspect, for: __MODULE__ do
-    # We define a custom inspect implementation to avoid printing key details to
-    # logs or anything.
-    import Inspect.Algebra
+  # defimpl Inspect, for: __MODULE__ do
+  #   # We define a custom inspect implementation to avoid printing key details to
+  #   # logs or anything.
+  #   import Inspect.Algebra
 
-    def inspect(_auth, _opts) do
-      concat(["#BasicAuth<...>"])
-    end
-  end
+  #   def inspect(_auth, _opts) do
+  #     concat(["#BasicAuth<...>"])
+  #   end
+  # end
 end
